25d1db11d4df696d15797b74c197cc5a64dff433
Reshma Abdul Rahim <reshmarahim.abdul@microsoft.com>
2025-04-03 12:37:36 -0400

    Updates
    Signed-off-by: Reshma Abdul Rahim <reshmarahim.abdul@microsoft.com>

---
+++ main.tf
@@ -0,0 +1,95 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0"
+    }
+    postgresql = {
+      source  = "cyrilgdn/postgresql"
+      version = "1.22.0"
+      configuration_aliases = [postgresql.pgdb-test]
+    }
+  }
+}
+
+resource "kubernetes_deployment" "postgres" {
+  metadata {
+    name      = "postgres"
+    namespace = var.context.runtime.kubernetes.namespace
+  }
+  spec {
+    selector {
+      match_labels = {
+        app = "postgres"
+        resource = context.resource.name
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "postgres"
+          resource: context.resource.name
+        }
+      }
+      spec {
+        container {
+          image = "ghcr.io/radius-project/mirror/postgres:latest"
+          name  = "postgres"
+          env {
+            name  = "POSTGRES_PASSWORD"
+            value = var.password
+          }
+          port {
+            container_port = 5432
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "postgres" {
+  metadata {
+    name      = "postgres"
+    namespace = var.context.runtime.kubernetes.namespace
+  }
+  spec {
+    selector = {
+      app = "postgres"
+    }
+    port {
+      port        = 5432
+      target_port = 5432
+    }
+  }
+}
+
+resource "time_sleep" "wait_120_seconds" {
+  depends_on = [kubernetes_service.postgres]
+  create_duration = "120s"
+}
+
+resource postgresql_database "pg_db_test" {
+  provider = postgresql.pgdb-test
+  depends_on = [time_sleep.wait_120_seconds]
+  name = "pg_db_test"
+}
+
+output "result" {
+  value = {
+    values = {
+      host = "${kubernetes_service.metadata.name}.${kubernetes_service.metadata.namespace}.svc.cluster.local"
+      port = kubernetes_service.spec.port[0].port
+    }
+    secrets = {
+      password = kubernetes_service.metadata.password
+    }
+    // UCP resource IDs
+    resources = [
+        "/planes/kubernetes/local/namespaces/${kubernetes_service.metadata.namespace}/providers/core/Service/${kubernetes_service.metadata.name}",
+        "/planes/kubernetes/local/namespaces/${kubernetes_deployment.metadata.namespace}/providers/apps/Deployment/${kubernetes_deployment.metadata.name}"
+    ]
+  }
+  description = "The result of the Recipe. Must match the target resource's schema."
+  sensitive = true
+}
