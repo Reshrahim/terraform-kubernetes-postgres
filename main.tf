03ba9ab3eb1186d00d8550c5a385758b332ddf49
Reshma Abdul Rahim <reshmarahim.abdul@microsoft.com>
2025-04-04 13:59:09 -0400

    Updates
    Signed-off-by: Reshma Abdul Rahim <reshmarahim.abdul@microsoft.com>

--- main.tf
+++ main.tf
@@ -95,10 +95,5 @@ output "result" {
       username = "postgres"
       password = var.password
     }
-    // UCP resource IDs
-    resources = [
-        "/planes/kubernetes/local/namespaces/${kubernetes_service.metadata.namespace}/providers/core/Service/${kubernetes_service.metadata.name}",
-        "/planes/kubernetes/local/namespaces/${kubernetes_deployment.metadata.namespace}/providers/apps/Deployment/${kubernetes_deployment.metadata.name}"
-    ]
   }
 }
