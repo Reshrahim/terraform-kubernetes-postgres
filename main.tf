8762190cef0034b54792cc2316edf2742ed70a03
Reshma Abdul Rahim <reshmarahim.abdul@microsoft.com>
2025-04-15 12:58:37 -0400

    updates
    Signed-off-by: Reshma Abdul Rahim <reshmarahim.abdul@microsoft.com>

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "kubernetes_deployment" "postgres" {
           }
 
           port {
-            container_port = 5432
+            container_port = 5433
           }
         }
       }
@@ -71,8 +71,8 @@ resource "kubernetes_service" "postgres" {
     }
 
     port {
-      port        = 5432
-      target_port = 5432
+      port        = 5433
+      target_port = 5433
     }
   }
 }
@@ -91,7 +91,7 @@ output "result" {
   value = {
     values = {
       host = "${kubernetes_service.postgres.metadata[0].name}.${kubernetes_service.postgres.metadata[0].namespace}.svc.cluster.local"
-      port = "5432"
+      port = "5433"
       database = var.context.resource.name
       username = "postgres"
       password = var.password
